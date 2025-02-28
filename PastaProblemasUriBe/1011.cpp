fa21e28f9a08e82cc0f6b6f8507afd787bcf1b3c
05041994 <gabrielsanta279@gmail.com>
2025-02-28 15:13:38 -0300

    criando na pasta

---
+++ PastaProblemasUriBe/1011.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int Main(){
+
+  double raio, volume;
+  double pi = 3.14159;
+
+  cin >> raio;
+
+  volume = ( 4.0/3 ) * pi * ( raio * raio *raio );
+  
+  cout << "VOLUME = "<< fixed << setprecision(3) << volume << endl;
+  
+  return 0;
+}
