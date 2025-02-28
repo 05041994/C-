0071282c91615c20e34de9fe4f4e3ec35630e9fd
05041994 <gabrielsanta279@gmail.com>
2025-02-28 15:05:41 -0300

    problema 1011

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main(){
 
-  cout << "Oi";
+  double raio, volume;
+  double pi = 3.14159;
 
+  cin >> raio;
+
+  volume = ( 4.0/3 ) * pi * ( raio * raio *raio );
+  
+  cout << "VOLUME = "<< fixed << setprecision(3) << volume << endl;
+  
+  return 0;
 }
